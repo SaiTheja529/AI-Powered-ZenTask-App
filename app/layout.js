47afb77e0ac22a2587a41b41f1ed57e0e9ec1ba8
Sai Theja <p.sivasaitheja@gmail.com>
2025-03-07 19:40:18 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
 import { CopilotKit } from "@copilotkit/react-core"; 
 import "@copilotkit/react-ui/styles.css";
 import ITodo from "@/components/ITodo";
